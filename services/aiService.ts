ac54371ee322c97a17c95ce08422dd4dc151729a
rashik87 <rr7597372@gmail.com>
2025-09-11 21:17:37 +0100

    Update aiService.ts

--- services/aiService.ts
+++ services/aiService.ts
@@ -10,7 +10,7 @@ const getAi = (): GoogleGenAI => {
     const apiKey = process.env.GEMINI_API_KEY;
         console.log("Gemini Key:", process.env.GEMINI_API_KEY ? "Loaded ✅" : "Missing ❌");
         if (!apiKey) {
-            throw new Error("Gemini API Key not found. The API_KEY environment variable is not set.");
+            throw new Error("Gemini API Key not found. The GEMINI_API_KEY environment variable is not set.");
         }
         aiInstance = new GoogleGenAI({ apiKey });
     }
